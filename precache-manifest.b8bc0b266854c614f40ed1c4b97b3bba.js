74a27044f26caecba1602b9957f51e1c5fd7f4c0
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-04-09 14:59:46 -0400

    Updates

---
+++ precache-manifest.b8bc0b266854c614f40ed1c4b97b3bba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb8c833cc7ab129e4d533f7d6d62dc2",
+    "url": "/React_Portfolio/index.html"
+  },
+  {
+    "revision": "68f60dc30fdf5b2f5655",
+    "url": "/React_Portfolio/static/css/main.3bcbf5b1.chunk.css"
+  },
+  {
+    "revision": "2dd760b55282cebe3577",
+    "url": "/React_Portfolio/static/js/2.ee4a2327.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React_Portfolio/static/js/2.ee4a2327.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68f60dc30fdf5b2f5655",
+    "url": "/React_Portfolio/static/js/main.d332cea1.chunk.js"
+  },
+  {
+    "revision": "aeedd9b54d831ea4aef9",
+    "url": "/React_Portfolio/static/js/runtime-main.03dcee9b.js"
+  },
+  {
+    "revision": "f57298a4f5711ffcf385141f3429d5e0",
+    "url": "/React_Portfolio/static/media/donorspot.f57298a4.jpg"
+  },
+  {
+    "revision": "27455fef95fb5d1edeb1d208c6737041",
+    "url": "/React_Portfolio/static/media/greensmoke.27455fef.jpg"
+  }
+]);
